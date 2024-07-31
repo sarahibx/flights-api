1f24cef4230158be8e8613d375acd0a8bf1148ae
DESKTOP-8NIU7KR\DELL <arandomemail1999@gmail.com>
2024-07-31 10:49:10 +0300

    Caching and Rated

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -44,5 +44,8 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+        RateLimiter::for('exports', function (Request $request) {
+            return Limit::perMinute(10)->by($request->user()->id);
+        });
     }
 }
